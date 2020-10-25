8e9d6397d62e3f0b428a7e7f5116c4ddb23d76cf
bert0ld <51457778+bert0ld@users.noreply.github.com>
2020-10-25 18:33:00 +0300

    refactor: tab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-  const nums = 	[	
+    const nums = 	[	
   					'zero','one','two','three','four','five','six','seven','eight',
   					'nine','ten','eleven','twelve','thirteen','fourteen','fifteen',
   					'sixteen','seventeen','eighteen','nineteen'
